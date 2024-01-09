f17649b0a9811a788ae36432ab8d2e1b93bccf93
Alessandro Craciun <alessandro.craciun2@gmail.com>
2024-01-09 20:04:28 +0100

    Lezione 2

--- my-app/src/components/ListContainer.jsx
+++ my-app/src/components/ListContainer.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import TaskContainer from "./TaskContainer";
 import "../styles/inputStyle.css";
 
+import { Row, Col } from "react-grid-system";
+
 function ListContainer(props) {
 	const { tasks, label, checked, action } = props;
 
 	return (
 		<>
-			<p>{label}</p>
-			<TaskContainer tasks={tasks} checked={checked} action={action} />
+			<Row>
+				<Col sm={12}>
+					<p>{label}</p>
+				</Col>
+			</Row>
+
+			<Row>
+				<Col sm={12}>
+					<TaskContainer tasks={tasks} checked={checked} action={action} />
+				</Col>
+			</Row>
 		</>
 	);
 }
--- my-app/src/pages/toDo.jsx
+++ my-app/src/pages/toDo.jsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-grid-system";
+import { FaTrash, FaPen } from "react-icons/fa";
+import useLocalStorage from "use-local-storage";
+
+import "../styles/todoStyle.css";
+
 import Input from "../components/Input";
 import Button from "../components/Button";
 import Output from "../components/Output";
 import ListContainer from "../components/ListContainer";
-import "../styles/todoStyle.css";
 
 function ToDo() {
 	const [value, setValue] = useState("");
@@ -12,11 +17,18 @@ function ToDo() {
 	const [total, setTotal] = useState(0);
 	// const [allTask, setallTask] = useState("Add new task");
 
+	const [username, setUsername] = useLocalStorage("name", "");
+
 	useEffect(() => {
 		//quando [] è vuoto viene eseguito solo una volta
 		//al render del component dentro cui è inserito
 		console.log("Render del component");
+		setUsername("Mario");
 	}, []);
+
+	useEffect(() => {
+		console.log("username:", username);
+	}, [username]);
 
 	const onchangeHandler = (event) => {
 		setValue(event.target.value);
@@ -80,100 +92,137 @@ function ToDo() {
 	};
 
 	return (
-		<div>
+		<Container>
+			<Row>
+				<Col sm={3}></Col>
+				<Col sm={6}>
+					<h1>TITOLO</h1>
+				</Col>
+			</Row>
+			<Row>
+				<Col sm={3}></Col>
+				<Col sm={6}>
+					<ListContainer
+						tasks={newTask}
+						label="To Do"
+						checked={false}
+						action={complete}
+						//onChange={...}
+						//onEdit={...}
+						//onDelete={...}
+					/>
+				</Col>
+			</Row>
+			<Row>
+				<Col sm={3}></Col>
+				<Col sm={6}>
+					<ListContainer
+						tasks={newComplitedTask}
+						label="Completed"
+						checked={true}
+						action={uncomplete}
+					/>
+				</Col>
+			</Row>
 			<div>
-				<h1>-- Inizio nuovo codice --</h1>
-				<ListContainer
-					tasks={newTask}
-					label="To Do"
-					checked={false}
-					action={complete}
-					//onChange={...}
-					//onEdit={...}
-					//onDelete={...}
-				/>
-				<ListContainer
-					tasks={newComplitedTask}
-					label="Completed"
-					checked={true}
-					action={uncomplete}
-				/>
-				<h1>-- Fine nuovo codice --</h1>
-			</div>
-			<div className="todoContainer">
-				{newTask.length === 0 && newComplitedTask.length === 0 ? (
-					<h4 className="green">Add your first task</h4>
-				) : newTask.length === 0 && newComplitedTask.length > 0 ? (
-					<h4 className="red">All tasks completed</h4>
-				) : (
-					<h4 className="orange">{newTask.length} to complete</h4>
-				)}
+				<div>
+					<h1>-- Inizio nuovo codice --</h1>
+					<ListContainer
+						tasks={newTask}
+						label="To Do"
+						checked={false}
+						action={complete}
+						//onChange={...}
+						//onEdit={...}
+						//onDelete={...}
+					/>
+					<ListContainer
+						tasks={newComplitedTask}
+						label="Completed"
+						checked={true}
+						action={uncomplete}
+					/>
+					<h1>-- Fine nuovo codice --</h1>
+				</div>
+				<div className="todoContainer">
+					{newTask.length === 0 && newComplitedTask.length === 0 ? (
+						<h4 className="green">Add your first task</h4>
+					) : newTask.length === 0 && newComplitedTask.length > 0 ? (
+						<h4 className="red">All tasks completed</h4>
+					) : (
+						<h4 className="orange">{newTask.length} to complete</h4>
+					)}
 
-				<div className="inputContainer">
-					<Input onchange={onchangeHandler} value={value} />
-					<Button classname="btn" action={addTask} label="Add" />
+					<div className="inputContainer">
+						<Input onchange={onchangeHandler} value={value} />
+						<Button classname="btn" action={addTask} label="Add" />
+					</div>
+					<Output
+						label="To Do"
+						value={newTask.map((el) => (
+							<div className="inputChekbox">
+								<input type="checkbox" onChange={() => complete(el)} />
+								<span>{el}</span>
+
+								<div className="img-container">
+									<FaPen
+										color="#304d30"
+										size={24}
+										style={{ marginRight: "10px", cursor: "pointer" }}
+										onClick={() => editTask(el)}
+									/>
+									<FaTrash
+										color="#304d30"
+										size={24}
+										style={{ cursor: "pointer" }}
+										onClick={() => deleteTask(el)}
+									/>
+								</div>
+							</div>
+						))}
+					/>
 				</div>
 				<Output
-					label="To Do"
-					value={newTask.map((el) => (
+					label="Completed"
+					value={newComplitedTask.map((el) => (
 						<div className="inputChekbox">
-							<input type="checkbox" onChange={() => complete(el)} />
-							<span>{el}</span>
-
-							<div className="img-container">
-								<img
-									src="https://cdn-icons-png.flaticon.com/512/4100/4100751.png"
-									onClick={() => editTask(el)}
-								/>
-								<img
-									src="https://i.imgur.com/nwOid4Q.png"
-									onClick={() => deleteTask(el)}
-								/>
-							</div>
+							<input
+								type="checkbox"
+								name="task"
+								checked
+								onChange={() => uncomplete(el)}
+							/>
+							<span>
+								{" "}
+								<del>{el}</del>{" "}
+							</span>
 						</div>
 					))}
-				/>
-			</div>
-			<Output
-				label="Completed"
-				value={newComplitedTask.map((el) => (
-					<div className="inputChekbox">
-						<input
-							type="checkbox"
-							name="task"
-							checked
-							onChange={() => uncomplete(el)}
-						/>
-						<span>
-							{" "}
-							<del>{el}</del>{" "}
-						</span>
+				></Output>
+				total: {total}
+				<div className="contain-buttons">
+					<div className="btn-container">
+						<Button
+							classname="btn"
+							label="clear todo"
+							action={() => setnewTask([])}
+						></Button>
+						<Button
+							classname="btn"
+							label="clear completed"
+							action={() => setnewComplitedTask([])}
+						></Button>
 					</div>
-				))}
-			></Output>
-			total: {total}
-			<div className="contain-buttons">
-				<div className="btn-container">
-					<Button
-						classname="btn"
-						label="clear todo"
-						action={() => setnewTask([])}
-					></Button>
-					<Button
-						classname="btn"
-						label="clear completed"
-						action={() => setnewComplitedTask([])}
-					></Button>
-				</div>
-				<div>
-					<Button
-						classname="btn clear-all"
-						label="clear all taks"
-						action={() => clearAllTask()}
-					></Button>
+					<div>
+						<Button
+							classname="btn clear-all"
+							label="clear all taks"
+							action={() => clearAllTask()}
+						></Button>
+					</div>
 				</div>
 			</div>
-		</div>
+		</Container>
 	);
 }
 
