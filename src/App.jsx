b5744c7b8ca1c6b30266fc408d8227c139d84401
klementina1809 <klementina.ua@gmail.com>
2024-04-11 20:36:24 +0200

    Fix for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import useLocalStorage from "use-local-storage";
 
 import ToDo from "./pages/toDo";
@@ -32,7 +32,7 @@ function App() {
 		])
 	);
 
-	const router = createBrowserRouter([
+	const router = createHashRouter([
 		{
 			path: "/",
 			element: <ToDo categories={JSON.parse(categories)} />,
